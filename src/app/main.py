3adef9e62d5f0db7487a1584760076cf59e94971
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-08-14 13:36:20 +0000

    Рефакторинг по flake8

--- src/app/main.py
+++ src/app/main.py
@@ -1,119 +1,143 @@
-from fastapi import FastAPI, HTTPException, Depends, Query
-from typing import Optional
-from fastapi.security import OAuth2PasswordRequestForm
-import httpx
-from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Optional
+
+import httpx
+from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 # URLs микросервисов
-AUTH_SERVICE_URL = "http://auth_service:82"
-TRANSACTION_SERVICE_URL = "http://transactions_service:83"
+AUTH_SERVICE_URL = 'http://auth_service:82'
+TRANSACTION_SERVICE_URL = 'http://transactions_service:83'
+
+HTTP_STATUS_OK = 200
+HTTP_SERVICE_UNAVAILABLE = 503
+
+DETAIL_KEY = 'detail'
+
 
 class TransactionCreateRequest(BaseModel):
-    amount: float  # сумма должна быть положительной
-    type: str = Field(..., pattern=r"^(debit|credit)$")  # допустимые типы транзакций
+    """Модель для создания транзакции."""
+
+    amount: float
+    type: str = Field(..., pattern='(debit|credit)')
+
 
 class DateRangeRequest(BaseModel):
-    start: datetime = Field(..., description="Начало временного диапазона")
-    end: datetime = Field(..., description="Конец временного диапазона")
+    """Модель для указания временного диапазона."""
 
-# Регистрация пользователя
-@app.post("/register")
+    start: datetime = Field(..., description='Начало временного диапазона')
+    end: datetime = Field(..., description='Конец временного диапазона')
+
+
+async def post_request(client: httpx.AsyncClient, url: str, **kwargs):
+    """Универсальная функция для выполнения POST-запросов."""
+    response = await client.post(url, **kwargs)
+    if response.status_code == HTTP_STATUS_OK:
+        return response.json()
+    raise HTTPException(
+        status_code=response.status_code,
+        detail=response.json().get(DETAIL_KEY),
+    )
+
+
+@app.post('/register')
 async def register(
-    username: str = Query(...),
-    password: str = Query(...),
-    first_name: Optional[str] = Query(None),
-    last_name: Optional[str] = Query(None)
+    username: str = Query(default=None),
+    password: str = Query(default=None),
+    first_name: Optional[str] = Query(default=None),
+    last_name: Optional[str] = Query(default=None),
 ):
+    """Регистрация нового пользователя."""
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f"{AUTH_SERVICE_URL}/register",
+        return await post_request(
+            client,
+            '{0}/register'.format(AUTH_SERVICE_URL),
             params={
-                "username": username,
-                "password": password,
-                "first_name": first_name,
-                "last_name": last_name
-            }
+                'username': username,
+                'password': password,
+                'first_name': first_name,
+                'last_name': last_name,
+            },
         )
-        if response.status_code == 200:
-            return response.json()
-        raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
 
-# Логин пользователя
-@app.post("/login")
+
+@app.post('/login')
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    """Логин пользователя."""
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    login_data = {
+        'username': form_data.username,
+        'password': form_data.password,
+    }
     async with httpx.AsyncClient() as client:
-        headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        data = {
-            "username": form_data.username,
-            "password": form_data.password,
-        }
-        response = await client.post(f"{AUTH_SERVICE_URL}/login", data=data, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
-
-# Создание транзакции
-@app.post("/transactions/")
-async def create_transaction(request: TransactionCreateRequest, token: str = Query(...)):
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f"{TRANSACTION_SERVICE_URL}/transaction/transactions/",
-            json={
-                "amount": request.amount,
-                "type": request.type
-            },
-            params={"token": token}  # Передача токена в микросервис
+        return await post_request(
+            client,
+            '{0}/login'.format(AUTH_SERVICE_URL),
+            data=login_data,
+            headers=headers,
         )
-        if response.status_code == 200:
-            return response.json()
-        raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
 
-# Получение отчета по транзакциям
-@app.post("/transactions/report/")
-async def get_transactions_report(request: DateRangeRequest, token: str = Query(...)):
+
+@app.post('/transactions/')
+async def create_transaction(
+    request: TransactionCreateRequest, token: Optional[str] = None,
+):
+    """Создание транзакции."""
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f"{TRANSACTION_SERVICE_URL}/transaction/transactions/report/",
-            json={
-                "start": request.start.isoformat(),
-                "end": request.end.isoformat()
-            },
-            params={"token": token}  # Передача токена в микросервис
+        return await post_request(
+            client,
+            '{0}/transaction/transactions/'.format(TRANSACTION_SERVICE_URL),
+            json={'amount': request.amount, 'type': request.type},
+            params={'token': token},
         )
-        if response.status_code == 200:
-            return response.json()
-        raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
 
-# Проверка здоровья аутентификационного сервиса
-async def check_auth_service_health():
+
+@app.post('/transactions/report/')
+async def get_transactions_report(
+    request: DateRangeRequest, token: Optional[str] = None,
+):
+    """Получение отчета по транзакциям за указанный временной диапазон."""
+    start_iso = request.start.isoformat()
+    end_iso = request.end.isoformat()
+
+    async with httpx.AsyncClient() as client:
+        return await post_request(
+            client,
+            '{0}/transaction/transactions/report/'.format(
+                TRANSACTION_SERVICE_URL,
+            ),
+            json={'start': start_iso, 'end': end_iso},
+            params={'token': token},
+        )
+
+
+async def check_service_health(url: str) -> bool:
+    """Универсальная функция проверки состояния сервиса."""
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(f"{AUTH_SERVICE_URL}/healthz/ready")
+            response = await client.get(url)
             response.raise_for_status()
-            return response.status_code == 200
+            return response.status_code == HTTP_STATUS_OK
         except httpx.RequestError:
             return False
 
-# Проверка здоровья сервиса транзакций
-async def check_transaction_service_health():
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{TRANSACTION_SERVICE_URL}/transaction/healthz/ready")
-            response.raise_for_status()
-            return response.status_code == 200
-        except httpx.RequestError:
-            return False
 
-# Проверка состояния всех сервисов
-@app.get("/healthz/ready")
+@app.get('/healthz/ready')
 async def health_check():
-    auth_healthy = await check_auth_service_health()
-    transaction_healthy = await check_transaction_service_health()
+    """Проверка состояния всех сервисов."""
+    auth_url = '{0}/healthz/ready'.format(AUTH_SERVICE_URL)
+    trans_url = '{0}/transaction/healthz/ready'.format(TRANSACTION_SERVICE_URL)
+
+    auth_healthy = await check_service_health(auth_url)
+    transaction_healthy = await check_service_health(trans_url)
 
     if not auth_healthy or not transaction_healthy:
-        raise HTTPException(status_code=503, detail="One or more dependencies are not available")
+        raise HTTPException(
+            status_code=HTTP_SERVICE_UNAVAILABLE,
+            detail='Один или несколько сервисов недоступны',
+        )
 
-    return {"status": "healthy"}
+    return {'status': 'healthy'}
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,44 +1,108 @@
 import pytest
 from fastapi.testclient import TestClient
-from myapp import app  # Импортируйте ваш FastAPI объект
+from httpx import Response, Request
+from src.app.main import app
 
 client = TestClient(app)
 
-# Мокируем успешное состояние сервисов
-@pytest.fixture
-def mock_services(mocker):
-    mocker.patch("httpx.AsyncClient.get", side_effect=lambda url: {
-        "http://auth_service:82/healthz/ready": {"status": "healthy"},
-        "http://transactions_service:83/healthz/ready": {"status": "healthy"}
-    }.get(url, {}))
 
-def test_health_check_healthy(mock_services):
+@pytest.fixture
+def mock_auth_service(mocker):
+    async def mock_post(url, *, data=None, json=None, params=None, headers=None):
+        if url == "http://auth_service:82/register":
+            if params and params.get("username") == "existing_user":
+                return Response(status_code=400, request=Request("POST", url), text='{"detail": "User already exists"}')
+            return Response(status_code=200, request=Request("POST", url), text='{"message": "User registered successfully"}')
+
+        if url == "http://auth_service:82/login":
+            # Проверка data вместо params
+            if data and data.get("username") == "invalid_user":
+                return Response(status_code=400, request=Request("POST", url), text='{"detail": "Invalid credentials"}')
+            return Response(status_code=200, request=Request("POST", url), text='{"access_token": "valid_token"}')
+
+    mocker.patch('httpx.AsyncClient.post', side_effect=mock_post)
+
+
+@pytest.fixture
+def mock_transaction_service(mocker):
+    async def mock_post(url, json=None, params=None):
+        if url == "http://transactions_service:83/transaction/transactions/":
+            if params and params.get("token") == "invalid_token":
+                return Response(status_code=401, text='{"detail": "Invalid token"}')
+            return Response(status_code=200, text='{"message": "Transaction created"}')
+
+        if url == "http://transactions_service:83/transaction/transactions/report/":
+            return Response(status_code=200, text='{"report": "Some report data"}')
+
+    mocker.patch('httpx.AsyncClient.post', side_effect=mock_post)
+
+
+@pytest.fixture
+def mock_health_check(mocker):
+    async def mock_get(url, *args, **kwargs):
+        if url == "http://transactions_service:83/transaction/healthz/ready":
+            # Создаем корректный объект Request для Response
+            request = Request("GET", url)
+            return Response(status_code=200, text='{"status": "healthy"}', request=request)
+        if url == "http://auth_service:82/healthz/ready":
+            # Создаем корректный объект Request для Response
+            request = Request("GET", url)
+            return Response(status_code=200, text='{"status": "healthy"}', request=request)
+        return Response(status_code=404, text='{"detail": "Not Found"}', request=Request("GET", url))
+
+    mocker.patch('httpx.AsyncClient.get', side_effect=mock_get)
+
+
+def test_register_user_success(mock_auth_service):
+    response = client.post("/register", params={"username": "new_user",
+                           "password": "password123", "first_name": "Vladislav", "last_name": "Zarubin"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "User registered successfully"}
+
+
+def test_register_user_already_exists(mock_auth_service):
+    response = client.post(
+        "/register", params={"username": "existing_user", "password": "password123"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "User already exists"}
+
+
+def test_login_user_success(mock_auth_service):
+    response = client.post(
+        "/login", data={"username": "valid_user", "password": "password123"})
+    assert response.status_code == 200
+    assert response.json() == {"access_token": "valid_token"}
+
+
+def test_login_user_invalid_credentials(mock_auth_service):
+    response = client.post(
+        "/login", data={"username": "invalid_user", "password": "password123"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid credentials"}
+
+
+def test_create_transaction_success(mock_transaction_service):
+    response = client.post(
+        "/transactions/", json={"amount": 100.0, "type": "debit"}, params={"token": "valid_token"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Transaction created"}
+
+
+def test_create_transaction_invalid_token(mock_transaction_service):
+    response = client.post(
+        "/transactions/", json={"amount": 100.0, "type": "debit"}, params={"token": "invalid_token"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid token"}
+
+
+def test_get_transactions_report_success(mock_transaction_service):
+    response = client.post("/transactions/report/", json={
+                           "start": "2024-01-01T00:00:00", "end": "2024-01-31T23:59:59"}, params={"token": "valid_token"})
+    assert response.status_code == 200
+    assert response.json() == {"report": "Some report data"}
+
+
+def test_health_check_success(mock_health_check):
     response = client.get("/healthz/ready")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
-
-# Мокируем неуспешное состояние одного из сервисов
-@pytest.fixture
-def mock_services_with_failure(mocker):
-    mocker.patch("httpx.AsyncClient.get", side_effect=lambda url: {
-        "http://auth_service:82/healthz/ready": {"status": "healthy"},
-        "http://transactions_service:83/healthz/ready": None  # Имитация ошибки
-    }.get(url, {}))
-
-def test_health_check_with_failure(mock_services_with_failure):
-    response = client.get("/healthz/ready")
-    assert response.status_code == 503
-    assert response.json() == {"detail": "One or more dependencies are not available"}
-
-# Мокируем неуспешное состояние обоих сервисов
-@pytest.fixture
-def mock_services_with_all_failures(mocker):
-    mocker.patch("httpx.AsyncClient.get", side_effect=lambda url: {
-        "http://auth_service:82/healthz/ready": None,  # Имитация ошибки
-        "http://transactions_service:83/healthz/ready": None  # Имитация ошибки
-    }.get(url, {}))
-
-def test_health_check_with_all_failures(mock_services_with_all_failures):
-    response = client.get("/healthz/ready")
-    assert response.status_code == 503
-    assert response.json() == {"detail": "One or more dependencies are not available"}
